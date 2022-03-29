1ce091acb7376a9ad65a99f4bb4eb0bebe8939fa
Winston Limanto <wmlimanto@gmail.com>
2022-03-28 18:44:57 -0700

    added assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if ((eqArrays(firstArray, secondArray)) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3, 4], [1, 2]);
