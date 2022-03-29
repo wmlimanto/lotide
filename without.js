de111ada9182bb89f3c87fb4b290f4148ffe51ee
Winston Limanto <wmlimanto@gmail.com>
2022-03-28 19:23:40 -0700

    remove unwanted elements from an array

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if ((eqArrays(firstArray, secondArray)) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  }
+};
+
+//Implement without which will return a subset of a given array, removing unwanted elements.
+
+const without = function(array, removeUnwanted) {
+  const newArray = array.filter(function(element) {
+    if (!removeUnwanted.includes(element)) {
+      return element;
+    }
+  })
+  return newArray;
+};
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+console.log(without([5, 4, 3], [4, 3])) // => [5]
