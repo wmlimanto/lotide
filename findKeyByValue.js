20377fe7134abd43438325fe1785525bea39d95f
Winston Limanto <wmlimanto@gmail.com>
2022-03-30 21:52:21 -0700

    function that takes in an object and value; it scans the object and returns the first key that contains the given value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,12 +8,12 @@ const assertEqual = function(actual, expected) {
 
 //findKeyByValue takes in an object and a value. it should scan the object and return the first key which contains the given value. if no key with that given value is found, then it should return undefind. 
 
-const findKeyByValue = function(object, value) {
+const findKeyByValue = function(object, value) { 
   
-  const results = Object.keys(object);
+  const results = Object.keys(object); //put the keys of the object in an array called results
 
-  for (const key of results) {
-    if (object[key] === value) {
+  for (const key of results) { //for the key within results
+    if (object[key] === value) { //if the key of the object === the value
       return key;
     }
   } 
