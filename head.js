b749c0150140a203872f4264d21a4720f0e8b035
Winston Limanto <wmlimanto@gmail.com>
2022-04-12 22:16:42 -0700

    modularizing assertEqual.js, refactored head.js, and separated tests for both files

--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const head = function(array) {
   return array[0];
 }
 
-assertEqual(head([7,6,5]), 5); //compares element 0 in head array (7) vs expected value in assertEqual function (5) 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //compares element 0 in head array (Hello) vs expected value in assertEqual function (Hello)
-assertEqual(head([]), 5); //empty array should return undefined
+module.exports = head;
